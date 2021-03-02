a01c71ac3849c97210d1ec3e1a80f0e66584416a
Diego Abregu <alonzoatinoco@gmail.com>
2021-03-02 00:40:43 -0500

    se agrego en gulpgile una funcion para minificar imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 const { series, src, dest, watch } = require('gulp');
 //dest--> ubicacion donde se guardara
 const sass = require('gulp-dart-sass');
+const imagemin = require('gulp-imagemin');
 
 //Funcion que compila SASS
 
@@ -16,12 +17,18 @@ function minificarcss() {
         }) )
         .pipe( dest('./build/css') )
 }
+function imagenes() {
+    return src('src/img/**/*')
+        .pipe( imagemin() )
+        .pipe( dest( './build/img' ) )
+}
 function watchArchivo() {
     watch( 'src/scss/**/*.scss', css )//* la carpeta actual -- ** todos los archivos con la extension
 }
 
 exports.css = css;//compilador del sass al css
 exports.minificarcss = minificarcss;//minificador
+exports.imagenes = imagenes;
 exports.watchArchivo = watchArchivo;//compilador automatico
 
 
